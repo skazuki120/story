3870ff8127d89bee2cbc9628a46ac3453b9dd2e3
skazuki120 <sk8350250@outlook.jp>
2021-07-04 17:21:43 +0900

    Initial Commit

---
+++ story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  story
+//
+//  Created by 小路一樹 on 2021/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
